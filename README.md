c2db185e63ed775fe55db596503cbc97132eeaee
Denise <denise.damico33@gmail.com>
2023-04-13 23:21:52 -0300

    2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Esta es una api simple para consultar los docentes.
 
-Para corres este proyecto siga los siguiente pasos:
+Para correr este proyecto siga los siguiente pasos:
 
 - `npm i`
 - `npm start`
--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 3000;
 const alumnosRouter = require('./routes/alumnos.route')
-const aularRouter = require('./routes/aulas.routes')
+const aulasRouter = require('./routes/aulas.routes')
+const docentesRouter = require('./routes/docentes.route')
 
 app.use(express.json())
 app.use('/alumnos', alumnosRouter.router)
-app.use('/aulas', aularRouter.router )
+app.use('/aulas', aulasRouter.router )
+app.use('/docentes', docentesRouter.router )
 
 app.listen(PORT, ()=>{console.log(`App lista escuhando en el puerto ${PORT}`)} )
 
